85b66f39fc466f364621ec6f8f70b07517fe6b70
Brian Feldman <brian.feldman42@gmail.com>
2020-08-24 15:23:40 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,9 @@
 def square_array(array)
+  new_array = []
+  index = 0
   size = array.length
-  while array[size] do
-    array[size]
+  while index < size do
+    new_array << (array[index] ** 2)
+  end
+  new_array
 end
